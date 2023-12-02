5271cd4876916adf5f3f98fa4439185e39d61427
Ryam <ryanngohenrui@gmail.com>
2023-12-02 21:10:02 +0800

    Update Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,15 +1,19 @@
-import Container from "../Container";
-import Logo from "./Logo";
-import Search from "./Search";
+'use client';
+
+import Container from '../Container';
+import Logo from './Logo';
+import Search from './Search';
+import UserMenu from './UserMenu';
 
 const Navbar = () => {
   return (
-    <div className="fixed bg-white w-full z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]">
+    <div className="fixed z-10 w-full bg-white shadow-sm">
+      <div className="border-b-[1px] py-4">
         <Container>
-          <div className="flex flex-row justify-between items-center gap-3 md:gap-0">
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>
