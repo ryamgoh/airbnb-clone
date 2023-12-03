c3b41d751224c1002984d09416b56514021cda1e
Ryam <ryanngohenrui@gmail.com>
2023-12-03 11:30:41 +0800

    Add use client

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,19 +1,13 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const Logo = () => {
   const router = useRouter();
 
   return (
-    <Image
-      alt="logo"
-      className="hidden md:block cursor-pointer"
-      height="100"
-      width="100"
-      src="/images/logo.png"
-    />
+    <Image alt="logo" className="hidden cursor-pointer md:block" height="100" width="100" src="/images/logo.png" />
   );
 };
 
