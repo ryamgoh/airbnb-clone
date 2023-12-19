c09e506b9123b0cf934c042a57c3ea4f1975e50a
Ryam <ryanngohenrui@gmail.com>
2023-12-19 21:54:03 +0800

    Add Listing POST Route and Price

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -26,7 +26,17 @@ const Input = ({
 }: InputProps) => {
   return (
     <div className="relative w-full">
-      {formatPrice && <BiDollar size={24} absolute top-5 left-2 />}
+      {formatPrice && (
+        <BiDollar
+          size={24}
+          className="
+            absolute
+            left-2
+            top-5
+            text-neutral-700
+          "
+        />
+      )}
       <input
         id={id}
         disabled={disabled}
@@ -34,12 +44,12 @@ const Input = ({
         placeholder=" "
         type={type}
         className={`
-          peer 
-          w-full 
-          rounded-md 
+          peer
+          w-full
+          rounded-md
           border-2 
-          bg-white
-          p-4
+          bg-white 
+          p-4 
           pt-6
           font-light
           outline-none
@@ -49,24 +59,25 @@ const Input = ({
           ${formatPrice ? 'pl-9' : 'pl-4'}
           ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
           ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
-          `}
+        `}
       />
       <label
         className={`
-          text-md
-          absolute 
+          text-md 
+          absolute
           top-5 
           z-10 
           origin-[0] 
           -translate-y-3 
-          transform
+          transform 
           duration-150 
           ${formatPrice ? 'left-9' : 'left-4'}
-          peer-placeholder-shown:translate-y-0
-          peer-placeholder-shown:scale-100
+          peer-placeholder-shown:translate-y-0 
+          peer-placeholder-shown:scale-100 
           peer-focus:-translate-y-4
           peer-focus:scale-75
-          ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}}`}
+          ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
+        `}
       >
         {label}
       </label>
