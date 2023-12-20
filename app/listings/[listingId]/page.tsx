b8bf78e280520def272a70d98a4bcab62995b6b9
Ryam <ryanngohenrui@gmail.com>
2023-12-20 19:30:31 +0800

    Add Reservation

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -2,7 +2,6 @@ import getCurrentUser from '@/app/actions/getCurrentUser';
 import getListingById from '@/app/actions/getListingById';
 import ClientOnly from '@/app/components/ClientOnly';
 import EmptyState from '@/app/components/EmptyState';
-import React from 'react';
 import ListingClient from './ListingClient';
 
 interface IParams {
